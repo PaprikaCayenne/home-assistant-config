4f88195257ba68a53107bc97c8811829f13f395d
PaprikaCayenne <paprika.cayenne33@gmail.com>
2025-08-08 02:00:00 -0500

    🕒 Nightly backup: 2025-08-08 02:00:00

--- custom_components/xtend_tuya/ha_tuya_integration/tuya_integration_imports_no_cc.py
+++ custom_components/xtend_tuya/ha_tuya_integration/tuya_integration_imports_no_cc.py
@@ -32,10 +32,19 @@ from homeassistant.components.tuya.cover import (
     TuyaCoverEntity as TuyaCoverEntity,
     TuyaCoverEntityDescription as TuyaCoverEntityDescription,
 )
-from homeassistant.components.tuya.event import (
-    EVENTS as EVENTS_TUYA, # noqa: F401
-    TuyaEventEntity as TuyaEventEntity,
+from homeassistant.components.event import (
+    EventEntityDescription,
 )
+try:
+    from homeassistant.components.tuya.event import (
+        EVENTS as EVENTS_TUYA, # noqa: F401
+        TuyaEventEntity as TuyaEventEntity,
+    )
+except Exception:
+    EVENTS_TUYA: dict[str, tuple[EventEntityDescription, ...]] = {}
+    from homeassistant.components.event import (
+        EventEntity as TuyaEventEntity,  # noqa: F401
+    )
 from homeassistant.components.tuya.fan import (
     TUYA_SUPPORT_TYPE as FANS_TUYA,  # noqa: F401
     TuyaFanEntity as TuyaFanEntity,
@@ -103,7 +112,6 @@ from homeassistant.components.tuya.const import (
 )
 from homeassistant.components.tuya.entity import (
     TuyaEntity as TuyaEntity,
-    ElectricityTypeData as TuyaElectricityTypeData,  # noqa: F401
     EnumTypeData as TuyaEnumTypeData,  # noqa: F401
     IntegerTypeData as TuyaIntegerTypeData,  # noqa: F401
     _DPTYPE_MAPPING as TUYA_DPTYPE_MAPPING,  # noqa: F401
